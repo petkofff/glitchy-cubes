c9dcfbb90a8fbe6da1387476959a7f0deb2e24f8
petkofff <gpetkofff@abv.bg>
2016-01-03 16:23:08 +0200

    change color after collision, code style unified

--- src/js/main.js
+++ src/js/main.js
@@ -12,30 +12,50 @@ var Cube = window.Cube;
 
 var stats;
 
-function init (){
+function newColor (h, s, l) {
+	var color;
+	
+	if (h == null) {
+		h = 1;
+	}
+	
+	if (s == null) {
+		s = 1;
+	}
+	
+	if (l == null) {
+		l = 0.85;
+	}
+	
+	color = new THREE.Color();
+	color.setHSL(Math.random()*h, s, l);
+	return color;
+}
+
+function init () {
 	scene = new THREE.Scene();
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-	geometry = new THREE.BoxGeometry( 10, 10, 10);
+	geometry = new THREE.BoxGeometry(10, 10, 10);
 
-	material = new THREE.MeshPhongMaterial({color: colors[Math.floor(Math.random()*colors.length)], shininess: 5});
+	material = new THREE.MeshPhongMaterial({color: newColor(), shininess: 5});
 
-	light = new THREE.DirectionalLight( 0xffffff, 0.5 );
-	light.position.set( 0, 0, 20 );
-	scene.add( light );
+	light = new THREE.DirectionalLight(0xffffff, 0.5);
+	light.position.set(0, 0, 20);
+	scene.add(light);
 
 	//postprocessing
-	composer = new THREE.EffectComposer( renderer );
-	composer.addPass( new THREE.RenderPass( scene, camera ) );
+	composer = new THREE.EffectComposer(renderer);
+	composer.addPass(new THREE.RenderPass(scene, camera));
 	glitchPass = new THREE.GlitchPass(10,10);
 	//glitchPass.renderToScreen = true;
-	glitchPass.goWild=false;
-	composer.addPass( glitchPass );
+	glitchPass.goWild = false;
+	composer.addPass(glitchPass);
 
 	filmEffect = new THREE.FilmPass(0.9,0.27,7000,0.3);
 	filmEffect.renderToScreen = true;
@@ -44,8 +64,8 @@ function init (){
 	camera.position.z = 400;
 
 	var i = 0;
-	for (;i<2500;i++){
-		cubes.push(new Cube(geometry, material,{
+	for (;i<2500;i++) {
+		cubes.push(new Cube(geometry, material, {
 			x: Math.random()*400-200,
 			y: Math.random()*400-200,
 			z: -Math.random()*1500,
@@ -63,7 +83,7 @@ function init (){
 	}
 	
 	stats = new Stats();
-	stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
+	stats.setMode(0); // 0: fps, 1: ms, 2: mb
 
 	// align top-left
 	stats.domElement.style.position = 'absolute';
@@ -74,20 +94,24 @@ function init (){
 
 }
 
-init ();
+init();
 
-function updateCubes (cubes, camera){
-	
-	cubes.forEach(function(cube, index, array){
+function updateCubes (cubes, camera) {
+	cubes.forEach(function(cube, index, cubes) {
 
-		cube.move()
+		cube.move();
 
 		if (glitchPass.goWild === false && cube.colidesWithCamera(camera) === true) {
-			glitchPass.goWild=true;
+			glitchPass.goWild = true;
 
-			setTimeout(function(){
-				glitchPass.goWild=false;
-			}, Math.random()*500 + 500);
+			setTimeout(function() {
+				glitchPass.goWild = false;
+			}, Math.random()*500+500);
+			
+			var color = newColor();
+			cubes.forEach(function (cube, index, cubes) {
+				cube.mesh.material.color = color;
+			});
 		}
 
 		var removeAfter = 100;
@@ -95,7 +119,7 @@ function updateCubes (cubes, camera){
 			cube.set({
 				x: Math.random()*400-200,
 				y: Math.random()*400-200,
-				z: -1500 + camera.position.z + removeAfter,
+				z: -1500+camera.position.z+removeAfter,
 				speed: {
 					z: Math.random()+7
 				},
@@ -109,17 +133,14 @@ function updateCubes (cubes, camera){
 }
 
 var render = function () {
-  requestAnimationFrame(render);
-  
-  stats.begin();
-  
-	camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05 ;
+	requestAnimationFrame(render);
 
-	//light.position.x = camera.position.x;
-	//light.position.y = camera.position.y;
+	stats.begin();
 
-	updateCubes (cubes, camera);
+	camera.position.x += (mouseX-camera.position.x)*0.05;
+	camera.position.y += (-mouseY-camera.position.y)*0.05;
+
+	updateCubes(cubes, camera);
 	
 	stats.end();
 	
@@ -129,16 +150,16 @@ var render = function () {
 
 render();
 
-document.onmousemove = function(e){
-  var k = 683;
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
-	mouseX = ( e.pageX - windowHalfX ) / 2 * (k/window.innerWidth);
-	mouseY = ( e.pageY - windowHalfY ) / 2 * (k/window.innerHeight);
+document.onmousemove = function(e) {
+	var k = 683;
+	var windowHalfX = window.innerWidth/2;
+	var windowHalfY = window.innerHeight/2;
+	mouseX = (e.pageX-windowHalfX)/2*(k/window.innerWidth);
+	mouseY = (e.pageY-windowHalfY)/2*(k/window.innerHeight);
 }
 
 window.onresize = function() {
- camera.aspect = window.innerWidth / window.innerHeight;
- camera.updateProjectionMatrix();
- renderer.setSize( window.innerWidth, window.innerHeight );
+	camera.aspect = window.innerWidth/window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
